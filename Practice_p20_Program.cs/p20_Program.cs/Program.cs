dd7f150a25a4cec749492b05f49deb274e6a73ab
Lynn Soler <lsoler002@msn.com>
2019-01-23 13:45:01 -0700

    p20_Basic_C#_Practice

---
+++ Practice_p20_Program.cs/p20_Program.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
