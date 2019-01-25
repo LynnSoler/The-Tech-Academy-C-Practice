927a2390cf6774058c6b7a045c98fab65800aa08
Lynn Soler <lsoler002@msn.com>
2019-01-25 14:28:40 -0700

    Class Instantiation w/optional integer
    com1.. more instantiation practice

---
+++ Class_Instantiate_p115/Class_Instantiate_p115/Integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Instantiate_p115
+{
+    class Integer
+    {
+        public int Add(int num, int num2)
+        {
+            return (num + num2);
+        }
+
+    }
+}
---
+++ Class_Instantiate_p115/Class_Instantiate_p115/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Instantiate_p115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an integer to do math operations with");
+            int valOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose a 2nd integer if you would like.. or press 'Enter' to continue without");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int val)) { val = Convert.ToInt32(input); }
+
+            Integer ex = new Integer();
+            Console.WriteLine(ex.Add(valOne, val));
+
+            Console.ReadLine();
+        }
+    }
+}
