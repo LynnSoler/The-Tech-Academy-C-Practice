4dcb04d20415cf4216558bad3edbe31ceb68b501
Lynn Soler <lsoler002@msn.com>
2019-01-24 19:26:18 -0700

    Class & Object Creation
    com1

---
+++ Class_Object_p111/Class_Object_p111/Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_p111
+{
+    class Integer //CLASS FILE
+    {
+        public int firstNum = 50;
+        public void Add(int val)
+        {
+            Console.WriteLine(firstNum + val);
+        }
+
+        public void Subtract (int val)
+        {
+            Console.WriteLine(firstNum - val);
+        }
+
+        public void Multiply (int val)
+        {
+            Console.WriteLine(firstNum * val);
+        }
+    }
+}
---
+++ Class_Object_p111/Class_Object_p111/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_p111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an integer (whole #) you would like to do math operations with.");
+            int valOne = Convert.ToInt32(Console.ReadLine());
+
+            Integer example = new Integer(); //OBJECT CREATION
+            example.Add(valOne);
+            example.Subtract(valOne);
+            example.Multiply(valOne);
+
+
+            Console.ReadLine();
+        }
+    }
+}
