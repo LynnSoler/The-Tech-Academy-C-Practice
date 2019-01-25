f4f333aaf1b99ef154169ad94975a32661191f11
Lynn Soler <lsoler002@msn.com>
2019-01-25 15:35:34 -0700

    Class Instantiation, Method w/Output Param's
    com1

---
+++ Class_Instantiate_Static_p117/Class_Instantiate_Static_p117/Integer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Instantiate_Static_p117
+{
+    class Integer
+    {
+        public void Divide(int num) //VOID METHOD TO OUTPUT INT
+        {
+            Console.WriteLine(num / 2); //DIVIDE PASSED INT BY 2
+        }
+
+        public void getVal(out int x, out int y) //OUTPUT PARAMETERS
+        {
+            Console.WriteLine("Please enter a value");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a 2nd value");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void var(int x)
+        {
+            Console.WriteLine("var(int x)");
+        }
+
+        public static void var(string y)
+        {
+            Console.WriteLine("var(string y)");
+        }
+
+    }
+}
---
+++ Class_Instantiate_Static_p117/Class_Instantiate_Static_p117/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Instantiate_Static_p117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an integer to divide by 2");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Integer ex = new Integer(); //INSTANTIATE CLASS "INTEGER"
+            ex.Divide(num); //DIVIDE INPUT BY 2
+            Console.Read();
+
+            Integer ex2 = new Integer(); //2ND INSTANTIATION FOR CLASS "INTEGER"
+            int a, b; 
+            ex2.getVal(out a, out b); //OUTPUT PARAMATERS
+            Console.WriteLine("After calling the method, value of a: {0}", a);
+            Console.WriteLine("After calling the method, value of b: {0}", b);
+            Console.ReadKey();
+
+            Integer.var("test");
+            Console.ReadLine();
+
+        }
+    }
+}
