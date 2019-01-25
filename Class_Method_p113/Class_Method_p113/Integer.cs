11e175607e5926f42f760ed173608dbc956ac9b6
Lynn Soler <lsoler002@msn.com>
2019-01-24 19:57:32 -0700

    Class_Method
    com1

---
+++ Class_Method_p113/Class_Method_p113/Integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_p113
+{
+    class Integer
+    {
+        public void Add(int val, int val2)
+        {
+            Console.WriteLine(100 - val);
+            Console.WriteLine(val2);
+        }
+    }
+}
---
+++ Class_Method_p113/Class_Method_p113/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_p113
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //4.) CALL METHOD & SPECIFY PARAMETERS BY NAME
+            Console.WriteLine("Choose an integer (whole #) to do math operations with");
+            int valOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose another integer to do math operations with");
+            int valTwo = Convert.ToInt32(Console.ReadLine());
+
+            Integer ex = new Integer();
+            ex.Add(valOne, valTwo);
+            
+            //3.) CALL METHOD IN THE CLASS & PASS IN 2 NUMS
+            ex.Add(1, 9);
+
+            Console.ReadLine();
+        }
+    }
+}
