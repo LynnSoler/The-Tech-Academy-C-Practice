403dc1b00308055133b972822f536f19d3ecb9eb
Lynn Soler <lsoler002@msn.com>
2019-01-25 16:11:50 -0700

    Inheritance app
    com1

---
+++ Inheritance_p120/Inheritance_p120/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_p120
+{
+    public class Person
+    {
+        //PROPERTIES
+        public string FirstName; // { get; set; }
+        public string LastName; // { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("First Name: " + FirstName);
+            Console.WriteLine("Last Name: " + LastName);
+
+            //OR...
+            //Console.WriteLine("Name: [" + FirstName + " " + LastName + "]");
+        }
+    }
+
+}
---
+++ Inheritance_p120/Inheritance_p120/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_p120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.FirstName = "Sample";
+            emp1.LastName = "Student";
+            emp1.SayName();
+            Console.ReadKey();
+        }
+
+    }
+}
