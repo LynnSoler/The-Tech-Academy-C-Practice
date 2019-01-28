22b5f65be06951c0aeecf4fa6939230bc98c1b68
Lynn Soler <lsoler002@msn.com>
2019-01-28 16:06:33 -0700

    DateTime basic practice
    com1

---
+++ Date_Time_p156/Date_Time_p156/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_p156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now; //PRINT CURRENT DATE/TIME TO CONSOLE
+            Console.WriteLine(now);
+            Console.WriteLine("Enter a whole number to see what time it will be in your chosen number of hours"); //ASK USER FOR NUM
+            TimeSpan Xhours = TimeSpan.FromHours(Convert.ToDouble(Console.ReadLine()));
+            DateTime inXHours = now + Xhours;
+            Console.WriteLine("The exact time will be: " + inXHours); //PRINT EXACT TIME IT WILL BE IN XHOURS (XHOURS = NUM USER PREVIOUSLY ENTERED)
+
+            Console.Read();
+        }
+    }
+}
