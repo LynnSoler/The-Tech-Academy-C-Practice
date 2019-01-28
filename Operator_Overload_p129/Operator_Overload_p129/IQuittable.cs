c081d821419bbef8872936582271096c5920c972
Lynn Soler <lsoler002@msn.com>
2019-01-27 19:05:38 -0700

    Revert "Revert "operator overload, chkg equality""
    This reverts commit 97b84ef6336af77d5f1ab01ab492bc3fc5a513c4.

---
+++ Operator_Overload_p129/Operator_Overload_p129/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overload_p129
+{
+    interface IQuittable
+    {
+        void Quit();
+       
+    }
+}
