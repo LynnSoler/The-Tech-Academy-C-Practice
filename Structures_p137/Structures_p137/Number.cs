9371291ff5eef191cfec9f2cd247bcb5c42751e5
Lynn Soler <lsoler002@msn.com>
2019-01-27 21:31:52 -0700

    Basic Structure Creation.. (replace "class", etc)
    com1

---
+++ Structures_p137/Structures_p137/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_p137
+{
+    public struct Number //STRUCT CREATION = OVERWRITE "CLASS" W/ "STRUCT"
+    {
+        public decimal Amount; //{ get; set; } //ASSIGN PROP "AMT" W/ DATA TYPE "DEC"
+    }
+}
---
+++ Structures_p137/Structures_p137/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_p137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number(); //OBJ CREATION "NUMBER" IN MAIN() METH
+            num.Amount = 234557690; //ASSIGN AMT
+            Console.WriteLine(num.Amount); //PRINT AMT TO CONSOLE
+            Console.Read();
+
+        }
+    }
+}
