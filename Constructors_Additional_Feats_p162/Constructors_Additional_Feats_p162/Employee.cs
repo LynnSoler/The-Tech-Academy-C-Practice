cecc83639fbaeb22b5039f9c5c9461753f313a99
Lynn Soler <lsoler002@msn.com>
2019-01-28 18:12:05 -0700

    Chaining 2 Constructors
    com1

---
+++ Constructors_Additional_Feats_p162/Constructors_Additional_Feats_p162/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Additional_Feats_p162
+{
+    public class Employee
+    {
+        //CONST VAR CREATION
+        const string color = "Blue";
+
+
+        public string First;
+        public string Last;
+        public int Id;
+
+        //CHAIN 2 CONSTRUCTORS TOGETHER
+        public Employee(string txt1) : this(txt1, "-", 0)
+        {
+            var First = txt1; //VAR CREATION USING "VAR"
+        }
+
+        public Employee(string txt1, string txt2) : this(txt1, txt2, 0)
+        {
+            First = txt1;
+            Last = txt2;
+        }
+
+        public Employee(string txt1, string txt2, int integer)
+        {
+            First = txt1;
+            Last = txt2;
+            Id = integer;
+        }
+
+        public void printName()
+        {
+            Console.WriteLine(Id + ":" + First + " " + Last);
+        }
+    }
+}
---
+++ Constructors_Additional_Feats_p162/Constructors_Additional_Feats_p162/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Additional_Feats_p162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ex = new Employee("Michelle", "Obama", 20);
+            ex.printName();
+
+            Console.ReadLine();
+        }        
+    }
+}
